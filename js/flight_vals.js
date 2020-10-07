81d0e71f2fc6127beca742b06b4daf6f6a770794
sid-flyarcher <62222390+sid-flyarcher@users.noreply.github.com>
2020-10-06 19:03:31 -0700

    Update flight_vals.js

--- js/flight_vals.js
+++ js/flight_vals.js
@@ -1,21 +1,21 @@
 export function GetFlightVals(){
 
     // Check to see if the counter has been initialized
-    if ( typeof increment == 'undefined' ) {
+    if ( typeof GetFlightVals.increment == 'undefined' ) {
         // It has not... perform the initialization
-        increment = 0;
+        GetFlightVals.increment = 0;
     }
 
 	var flight_vals = {
-	heading_deg: 180+180*Math.sin(increment/25), 
-    speed_knots: 80+80*Math.sin(increment/600), 
-    pressure_Pa: 1000+3*Math.sin(increment/50), 
-    altitude_ft: 1250+1250*Math.sin(increment/250), 
-    roll_deg: 180*Math.sin(increment/50), 
-    pitch_deg: 180*Math.sin(increment/50)
+	heading_deg: 180+180*Math.sin(GetFlightVals.increment/25), 
+    speed_knots: 80+80*Math.sin(GetFlightVals.increment/600), 
+    pressure_Pa: 1000+3*Math.sin(GetFlightVals.increment/50), 
+    altitude_ft: 1250+1250*Math.sin(GetFlightVals.increment/250), 
+    roll_deg: 180*Math.sin(GetFlightVals.increment/50), 
+    pitch_deg: 180*Math.sin(GetFlightVals.increment/50)
     };
 
-    ++increment
+    ++GetFlightVals.increment
   
   return flight_vals
 }
